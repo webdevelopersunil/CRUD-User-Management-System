98f3b6935397c4740c16d7fa0fae6da2bdafb2b4
webdevelopersunil <he.kingsmen@gmail.com>
2023-09-06 23:58:45 +0530

    Customer Created

--- server/controller/customerController.js
+++ server/controller/customerController.js
@@ -1,15 +1,61 @@
+const Customer = require('../models/Customer');
+const mongoose = require('mongoose');
+
 
 /**
  * GET/
  * Homepage
  */
-
 exports.homepage = async (req, res) => {    //async function for database stuff
 
     const locals = {
-        title : "Node Js",
-        description : "User Management System"
+        title: "Node Js",
+        description: "User Management System"
     }
-    
+
     res.render('index', locals);
+}
+
+
+/**
+ * GET/
+ * New Customer Form
+ */
+exports.addCustomer = async (req, res) => {    //async function for database stuff
+
+    const locals = {
+        title: "Add New Customer",
+        description: "User Management System"
+    }
+
+    res.render('customer/add', locals);
+}
+
+
+/**
+ * POST/
+ * Create New Customer
+ */
+exports.postCustomer = async (req, res) => {    //async function for database stuff
+
+    console.log(req.body);
+
+    const newCustomer = new Customer({
+
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        tel: req.body.tel,
+        email: req.body.email,
+        details: req.body.details
+
+    });
+
+    try {
+
+        await Customer.create(newCustomer);
+        res.redirect('/');
+
+    } catch (err) {
+        console.log(err);
+    }
 }
--- server/routes/customer.js
+++ server/routes/customer.js
@@ -7,5 +7,8 @@ const customerController = require('../controller/customerController');
  */
 router.get('/', customerController.homepage);
 
+router.get('/add', customerController.addCustomer);
+
+router.post('/add', customerController.postCustomer);
 
 module.exports = router;
