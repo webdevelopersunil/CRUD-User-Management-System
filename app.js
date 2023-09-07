07e209a02f561171662b86a1ee4db7da1ab4aa01
webdevelopersunil <he.kingsmen@gmail.com>
2023-09-07 12:28:53 +0530

    CRUD

--- app.js
+++ app.js
@@ -2,60 +2,52 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
-const flash = require('express-flash-message');
-
-// Expression Session
+const methodOverride = require('method-override');
+const { flash } = require('express-flash-message');
 const session = require('express-session');
+const connectDB = require('./server/config/db');
 
-const connectDB = require('./server/config/db');    // Require the db configuration
-// connect to the database
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connect to Database  
 connectDB();
 
-const port = 8000;
-const app = express();
-
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // Passing data
+app.use(express.json());
+app.use(methodOverride('_method'));
 
-app.use(express.static('public')); // serving images and static files
+// Static Files
+app.use(express.static('public'));
 
 // Express Session
 app.use(
-    session({
-      secret: 'secret',
-      resave: false,
-      saveUninitialized: true,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-      }
-    })
-  );
-  
-// Flash Messages Middleware
-// app.use(flash({ sessionKeyName: 'flashMessage' }));
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+    }
+  })
+);
 
+// Flash Messages
+app.use(flash({ sessionKeyName: 'flashMessage' }));
 
 // Templating Engine
 app.use(expressLayout);
-app.set('layout','./layouts/main');
+app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-
-app.use('/', require('./server/routes/customer'));
+// Routes
+app.use('/', require('./server/routes/customer'))
 
 // Handle 404
 app.get('*', (req, res) => {
-    res.status(404).render('404');
+  res.status(404).render('404');
 });
 
-
-
-app.listen(port, (err) => {
-    if(err){
-        console.log(`Server error : ${err}`);
-        return res.end('server stopped responding');
-    }
-    
-    console.log(`Server is running on the Port : ${port}`);
-
-})
+app.listen(port, ()=> {
+  console.log(`App listeing on port ${port}`)
+});
--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false); // To avoid strict mode in query
+const connectDB = async()=> {
+  try {
+    mongoose.set('strictQuery', false);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Database Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const connectDB = async() => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Database Connected: ${conn.connection.host}`);
-    }catch (error){
-        console.log(error);
-    }
-};
+
 
 module.exports = connectDB;
+
